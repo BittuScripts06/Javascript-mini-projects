5e76a472be3faec9727e6a55e26632d52a6018ce
Bittu Kumar <bk107161@gmail.com>
2025-03-07 16:23:19 +0530

    practice file deleted

--- LogIn form/practice/main1.js
+++
@@ -1,9 +0,0 @@
-let input =document.querySelector("input");
-let name=document.querySelector(".name-tag");
-name.innerText=localStorage.getItem("myName");
-
-input.addEventListener("input",(e)=>{
-localStorage.setItem("myName", e.target.value);
-name.innerText=localStorage.getItem("myName");
-})
-
